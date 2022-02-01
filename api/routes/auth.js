9c89ba990fdced2f34dff5e12c0ace8b10b93908
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2022-02-01 17:29:05 +0500

    added login route

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,23 +1,51 @@
 const User = require("../models/User");
-
+const CryptoJS = require("crypto-js");
 const router = require("express").Router();
 
-router.post("/register", async (req,res) => {
-    const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
+//Register user
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_KEY
+    ).toString(),
+  });
 
-    });
+  try {
+    const user = await newUser.save();
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-    try{
+//Login User
 
-        const user = await newUser.save();
-        res.status(201).json(user);
-    }catch(err){
-        res.status(500).json(err);
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if(!user){ 
+        res.status(401).json("Wrong Password or username!");
+        return;
     }
+    // Decrypt
+      
+    const bytes  = CryptoJS.AES.decrypt(user.password,process.env.SECRET_KEY );
+    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+    if(originalPassword !== req.body.password){ 
+     res.status(401).json("Wrong Password or username!");
+     return;
+    }
+    const {password,...info} = user._doc;
+
+    res.status(200).json(info);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
-
