1056d39db4a5e8bbab7d78f894d26fac832d73e9
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2022-02-02 21:58:03 +0500

    featured now uses api to get data using axios

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -1,53 +1,66 @@
 import { InfoOutlined, PlayArrow } from "@material-ui/icons";
-import { useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./featured.scss";
 
-function Featured({type}) {
-    useEffect(()=>{
-        const getRandom = async () =>{
+function Featured({ type }) {
+    const [content, setContent] = useState({});
 
+    useEffect(() => {
+        const getRandomContent = async () => {
+            try {
+                const res = await axios.get(`/movies/random?type${type}`,
+                    { headers: { token: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZjkxM2ZiZjA4OGE1MDZlZTQ4YzM5MSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MzcyNzg3OCwiZXhwIjoxNjQ0MTU5ODc4fQ.v5GNxzl9eT4HNhkvfPLyXuAUuZpQTxghdRMVLe0mpKo" } });
+
+
+                setContent(res.data[0]);
+            } catch (error) {
+                console.log(error);
+            }
         }
-    });
-  return (
-    <div className="featured">
-        { type && (
-            <div className="category">
-                <span>{type==="movies"?"Movies":"Series"}</span>
-                <select name="genre" id="genre">
-                    <option>Genre</option>
-                    <option value="adventure">Adventure</option>
-                    <option value="comedy">Comedy</option>
-                    <option value="crime">Crime</option>
-                    <option value="fantasy">Fantasy</option>
-                    <option value="historical">Historical</option>
-                    <option value="horror">Horror</option>
-                    <option value="romance">Romance</option>
-                    <option value="sci-fi">Sci-fi</option>
-                    <option value="thriller">Thriller</option>
-                    <option value="western">Western</option>
-                    <option value="animation">Animation</option>
-                    <option value="drama">Drama</option>
-                    <option value="documentary">Documentary</option>
-                </select>
-            </div>
-        )}
-        <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
-        <div className="info">
-            <img src="https://occ-0-1432-1433.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUZdeG1DrMstq-YKHZ-dA-cx2uQN_YbCYx7RABDk0y7F8ZK6nzgCz4bp5qJVgMizPbVpIvXrd4xMBQAuNe0xmuW2WjoeGMDn1cFO.webp?r=df1" alt="" />
-            <span className="desc">Lorem ipsum dolor sit, amet consectetur adipisicing elit. In nisi magnam necessitatibus veniam natus, iure, labore eos impedit quos reprehenderit, quasi asperiores dolor repudiandae tenetur tempore culpa fugiat eum corrupti.</span>
-            <div className="buttons">
-                <button className="play">
-                    <PlayArrow/>
-                    <span>Play</span>
-                </button>
-                <button className="more">
-                    <InfoOutlined/>
-                    <span>Info</span>
-                </button>
-            </div>
-        </div>
 
-    </div>
+        getRandomContent();
+    }, [type]);
+    return (
+        <div className="featured">
+            {type && (
+                <div className="category">
+                    <span>{type === "movies" ? "Movies" : "Series"}</span>
+                    <select name="genre" id="genre">
+                        <option>Genre</option>
+                        <option value="adventure">Adventure</option>
+                        <option value="comedy">Comedy</option>
+                        <option value="crime">Crime</option>
+                        <option value="fantasy">Fantasy</option>
+                        <option value="historical">Historical</option>
+                        <option value="horror">Horror</option>
+                        <option value="romance">Romance</option>
+                        <option value="sci-fi">Sci-fi</option>
+                        <option value="thriller">Thriller</option>
+                        <option value="western">Western</option>
+                        <option value="animation">Animation</option>
+                        <option value="drama">Drama</option>
+                        <option value="documentary">Documentary</option>
+                    </select>
+                </div>
+            )}
+            <img src={content.img} alt="" />
+            <div className="info">
+                <img src={content.imgTitle} alt="" />
+                <span className="desc">{content.desc}</span>
+                <div className="buttons">
+                    <button className="play">
+                        <PlayArrow />
+                        <span>Play</span>
+                    </button>
+                    <button className="more">
+                        <InfoOutlined />
+                        <span>Info</span>
+                    </button>
+                </div>
+            </div>
+
+        </div>
     );
 }
 
