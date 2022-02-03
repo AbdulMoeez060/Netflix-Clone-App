1bab25088d46c4b4ad82838e952d1ce159ee6c9c
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2022-02-03 12:22:33 +0500

    added authcontext nd login

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,7 +2,10 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./app.css";
 import Home from "./pages/home/Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./pages/login/Login";
+import { AuthContext } from "./context/authcontext/AuthContext";
+import { useContext } from "react";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
@@ -10,19 +13,21 @@ import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
 
-
 function App() {
-  
-
+  const { user } = useContext(AuthContext);
   return (
     <Router>
-      <Topbar />
-      <div className="container">
-        <Sidebar />
-        <Switch>
+      <Switch>
+      <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+      {user &&
+        <>
+        <Topbar />
+        <div className="container">
+          <Sidebar />
           <Route exact path="/">
             <Home />
           </Route>
+
           <Route path="/users">
             <UserList />
           </Route>
@@ -32,7 +37,7 @@ function App() {
           <Route path="/newUser">
             <NewUser />
           </Route>
-          <Route path="/products">
+          <Route path="/movies">
             <ProductList />
           </Route>
           <Route path="/product/:productId">
@@ -41,8 +46,9 @@ function App() {
           <Route path="/newproduct">
             <NewProduct />
           </Route>
-        </Switch>
-      </div>
+        </div></>}
+        
+      </Switch>
     </Router>
   );
 }
